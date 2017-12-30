593e389dd35377845dff37038ef8681c9534a7c5
himasagaratluri <himasagar.atluri@gmail.com>
2017-12-30 00:22:01 +0000

    edit jenkins default.rb

--- cookbooks/jenkins/recipes/default.rb
+++ cookbooks/jenkins/recipes/default.rb
@@ -4,3 +4,13 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+depends 'java'
+depends 'apt'
+
+package 'jenkins' do
+ action :upgrade
+end
+
+service 'jenkins' do
+ action [ :enable, :start]
+end
